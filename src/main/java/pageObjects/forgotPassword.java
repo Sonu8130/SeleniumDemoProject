177b9a4958df1d4977a82d452cc85446440b4972
sonu8130 <sonuag41@gmail.com>
2022-01-22 00:06:54 +0530

    2nd commit

---
+++ src/main/java/pageObjects/forgotPassword.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class forgotPassword {
+WebDriver driver;
+	
+public forgotPassword(WebDriver driver) {
+	this.driver=driver;
+}
+}
